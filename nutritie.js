388529997b50281289a93517f10128c3be6f9c89
mmp35 <magda.pall@yahoo.com>
2015-11-03 16:14:36 +0200

    update
    adfsafdas

--- nutritie.js
+++ nutritie.js
@@ -1,18 +1,15 @@
-<script type="text/javascript">
-
-						function  calculeazaIMC() {
-						  var greutate = document.getElementById('greutate').value
-						  var inaltime = document.getElementById('inaltime').value
-						  if (greutate == "") {
+function  calculeazaIMC() {
+		var greutate = document.getElementById('greutate').value
+		var inaltime = document.getElementById('inaltime').value
+				if (greutate == "") {
 							alert("Va rog introduceti greutatea.");
 							return;
 						  }
-						  if (inaltime == ""){ 
+				if (inaltime == ""){ 
 							alert("Va rog introduceti inaltimea.");
 							return;
 						  }
 						 
-						  var IMC = (greutate  / (inaltime * inaltime)) * 10000;
-						  document.getElementById('IMC').value=IMC.toFixed(2);
+		var IMC = (greutate  / (inaltime * inaltime)) * 10000;
+		document.getElementById('IMC').value=IMC.toFixed(2);
 						}
-</script>
